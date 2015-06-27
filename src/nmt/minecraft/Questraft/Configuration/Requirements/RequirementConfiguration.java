6266d23c9689fc355cc9704da5e2d00627b02470
Skyler <skymanzanares@hotmail.com>
2015-06-27 16:39:46 -0600

    Added switch for requirement type

--- src/nmt/minecraft/Questraft/Configuration/Requirements/RequirementConfiguration.java
+++ src/nmt/minecraft/Questraft/Configuration/Requirements/RequirementConfiguration.java
@@ -10,7 +10,32 @@ public abstract class RequirementConfiguration {
 	public abstract void fromConfig(ConfigurationSection config);
 	
 	public static RequirementConfiguration parseConfig(ConfigurationSection config) {
-		return null;
+		RequirementConfiguration req;
+		
+		String key = config.getKeys(false).iterator().next();
+		
+		switch (key) {
+		case "POSSESS":
+			req = new PossessRequirementConfiguration();
+			break;
+		case "VANQUISH":
+			req = new VanquishRequirementConfiguration();
+			break;
+		case "ARRIVE":
+			req = new ArriveRequirementConfiguration();
+			break;
+		case "POSITION":
+			req = new PositionRequirementConfiguration();
+			break;
+		default:
+			System.err.println("Unable to find matching requirement"
+					+ " key: " + key);
+			return null;
+		}
+		
+		req.fromConfig(config.getConfigurationSection(key));
+		
+		return req;
 	}
 	
 }
