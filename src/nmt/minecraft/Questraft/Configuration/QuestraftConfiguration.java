e3519869fad5cb7f503c8b4170174e8d09cbbea5
Skyler <skymanzanares@hotmail.com>
2015-06-27 14:47:28 -0600

    Branched into data storage

---
+++ src/nmt/minecraft/Questraft/Configuration/QuestraftConfiguration.java
@@ -0,0 +1,22 @@
+package nmt.minecraft.Questraft.Configuration;
+
+/**
+ * Top-level container for a quest configuration file
+ * @author Skyler
+ *
+ */
+public class QuestraftConfiguration {
+
+	public double configurationVersion;
+	
+	public String questName;
+	
+	public String description;
+	
+	public boolean savestate;
+	
+	public int fame;
+	
+	
+	
+}
