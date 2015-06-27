69e9a6973bea10039cde58618a408250e663c0b2
Skyler <skymanzanares@hotmail.com>
2015-06-27 15:06:43 -0600

    Created ending configuration template

---
+++ src/nmt/minecraft/Questraft/Configuration/EndConfiguration.java
@@ -0,0 +1,15 @@
+package nmt.minecraft.Questraft.Configuration;
+
+public abstract class EndConfiguration {
+	
+	private String type;
+	
+	protected EndConfiguration(String type) {
+		this.type = type;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+}
