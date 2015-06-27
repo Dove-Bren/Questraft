00c2e0ea0947a8677b86938d1627e302e3704951
Skyler <skymanzanares@hotmail.com>
2015-06-27 15:06:30 -0600

    Added other fields

--- src/nmt/minecraft/Questraft/Configuration/QuestraftConfiguration.java
+++ src/nmt/minecraft/Questraft/Configuration/QuestraftConfiguration.java
@@ -1,5 +1,12 @@
 package nmt.minecraft.Questraft.Configuration;
 
+import java.util.List;
+
+import nmt.minecraft.QuestManager.NPC.NPC;
+import nmt.minecraft.QuestManager.Quest.Goal;
+
+import org.bukkit.inventory.ItemStack;
+
 /**
  * Top-level container for a quest configuration file
  * @author Skyler
@@ -16,6 +23,16 @@ public class QuestraftConfiguration {
 	public boolean savestate;
 	
 	public int fame;
+	
+	private List<ItemStack> rewards;
+	
+	private List<Goal> goals;
+	
+	private List<NPC> npcs;
+	
+	private NPC startNPC;
+	
+	private EndConfiguration end;
 	
 	
 	
